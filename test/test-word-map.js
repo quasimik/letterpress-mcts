b8ca462041b1f0d1b777f525cfd6008c224ca577
Michael Liu <mike666234@gmail.com>
2017-10-03 01:16:43 +0800

    Add Mocha, add WordMap test

---
+++ test/test-word-map.js
@@ -0,0 +1,49 @@
+var assert = require('assert')
+var WordMap = require('../word-map.js')
+
+describe('WordMap', function() {
+
+    var wordMap = new WordMap()
+    it('returns all inserted plays', function() {
+        wordMap.insert('hi', [0,1])
+        wordMap.insert('hi', [0,2])
+        wordMap.insert('ho', [0,3])
+
+        assert.equal(wordMap.plays.length, 3)
+        assert.deepEqual(wordMap.plays[0], [0,1])
+        assert.deepEqual(wordMap.plays[1], [0,2])
+        assert.deepEqual(wordMap.plays[2], [0,3])
+    })
+
+    var wordMapCopy
+    it('copies all plays properly', function() {
+        wordMapCopy = wordMap.copy()
+
+        assert.equal(wordMap.plays.length, 3)
+        assert.deepEqual(wordMap.plays[0], [0,1])
+        assert.deepEqual(wordMap.plays[1], [0,2])
+        assert.deepEqual(wordMap.plays[2], [0,3])
+
+        assert.equal(wordMapCopy.plays.length, 3)
+        assert.deepEqual(wordMapCopy.plays[0], [0,1])
+        assert.deepEqual(wordMapCopy.plays[1], [0,2])
+        assert.deepEqual(wordMapCopy.plays[2], [0,3])
+    })
+
+    it('removes all variations of given word', function() {
+        assert(wordMap.remove('hi') === true)
+        assert.equal(wordMap.plays.length, 1)
+        assert.deepEqual(wordMap.plays[0], [0,3])
+    })
+    
+    it('removes words without affecting copied instances', function() {
+        assert.equal(wordMapCopy.plays.length, 3)
+        assert.deepEqual(wordMapCopy.plays[0], [0,1])
+        assert.deepEqual(wordMapCopy.plays[1], [0,2])
+        assert.deepEqual(wordMapCopy.plays[2], [0,3])
+    })
+    
+    it('remove nonexistent word: returns false', function() {
+        assert(wordMap.remove('hi') === false)
+    })
+})
---
+++ test/tests.js
@@ -0,0 +1,2 @@
+require('./test-word-map.js')
+require('./test-word-map-alt.js')
